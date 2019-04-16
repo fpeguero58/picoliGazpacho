1c310e2d3af287489dfa0bf163a746e02436787f
RicardoMilos <49229984+ElParoEsMalo@users.noreply.github.com>
2019-04-16 18:10:34 +0200

    cosa

--- src/modelo/vista/Factorias.java
+++ src/modelo/vista/Factorias.java
@@ -1,9 +1,13 @@
 package modelo.vista;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factorias {
 	private int demanda;
 	private int numeroTrabajadores;
 	private Tamano tamano;
+	List trabajadores=new ArrayList<Integer>();
 	public Factorias(int demanda, int numeroTrabajadores, Tamano tamano) {
 		super();
 		this.demanda = demanda;
