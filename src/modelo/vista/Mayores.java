79066797af41eac1a8be0c9f20df287ccc7a7e38
davidElGnomo69 <49229922+davidElGnomo69@users.noreply.github.com>
2019-04-30 09:44:50 +0200

    limpieza

--- src/modelo/vista/Mayores.java
+++ src/modelo/vista/Mayores.java
@@ -1,6 +1,6 @@
 package modelo.vista;
 
-public class Mayores extends Ser implements Subvencionable, Ahorrable, Empleable {
+public class Mayores extends Ser {
 	private float ahorros;
 	private float sueldo;
 
@@ -8,52 +8,25 @@ public class Mayores extends Ser implements Subvencionable, Ahorrable, Empleable
 		super(nombre, numeroIdentificacion, esperanzaVida);
 		this.setEdad(18);
 	}
-	
 
 	public float getAhorros() {
 		return ahorros;
 	}
 
-
 	public void setAhorros(int ahorros) {
 		this.ahorros = ahorros;
 	}
-
 
 	public float getSueldo() {
 		return sueldo;
 	}
 
-
 	public void setSueldo(float sueldo) {
 		this.sueldo = sueldo;
 	}
 
-
-	@Override
-	public float subvencionar(float subvencionEstado) {
-		if (this.ahorros >= (365-subvencionEstado)) {
-			this.ahorros-= 365-subvencionEstado;
-		}
-		else {
-			this.ahorros = 0;
-		}
-		return this.ahorros;
-	}
-
 	public void aumentarAhorros() {
-		 ahorros += getNecesidadVital() * 0.50;
-	}
-	public boolean edadJubilacion() {
-		return this.getEdad() >= 65;
-		// metodo en Estado que si este metodo es true, se elimine y cambie de
-		// coleccion.
+		ahorros += getNecesidadVital() * 0.50;
 	}
 
-
-	@Override
-	public float subvencionar() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 }
